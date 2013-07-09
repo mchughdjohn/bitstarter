d93e998e744ba59bd8833b2676cb3d5695dc59b9
John McHugh <jmchugh@yomiou.com>
2013-07-09 19:07:14 +0000

    Web.js update

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var fs = require('fs');
+var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8'); 
 
 var app = express.createServer(express.logger());
 
-fs.readFile('./index.html', function (err, html) {
-  if (err) throw err;
-  response.write(html);
-});
+app.get('/', function (request, response) {
+      response.send(buf.toString());
+});        
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
